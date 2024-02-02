0a4801c96d4562ddf59265d9ae91ae1eb0ac546d
Masahiro Yamanaka <yamanaka@cc.okayama-u.ac.jp>
2024-02-02 14:28:27 +0900

    Create json

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy download_image delete_image]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
 
   def new
@@ -14,19 +16,28 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    if @article.save
-      redirect_to @article
-    else
-      render :new, status: :unprocessable_entity
+    # if @article.save
+    #   redirect_to @article
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
 
     # pp "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", params[:article][:image_ids]
     # if params[:article][:image_ids]
@@ -37,17 +48,25 @@ class ArticlesController < ApplicationController
     #   params[:article].delete :image_ids
     # end
 
-    # # @article.images.attach(params[:images]) if @article.images.blank?
-    if @article.update(article_params)
-      # flash[:success] = "編集しました"
-      redirect_to @article
-    else
-      render :edit, status: :unprocessable_entity
+    # if @article.update(article_params)
+    #   # flash[:success] = "編集しました"
+    #   redirect_to @article
+    # else
+    #   render :edit, status: :unprocessable_entity
+    # end
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def download_image
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     image = @article.images.find(params[:image_id])
     file = image.blob.download
 
@@ -61,20 +80,34 @@ class ArticlesController < ApplicationController
   end
 
   def delete_image
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     image = @article.images.find(params[:image_id])
     image.purge
-    redirect_to edit_article_path(@article)
+
+    # redirect_to edit_article_path(@article)
+    respond_to do |format|
+      format.html { redirect_to edit_article_path(@article), notice: "Image was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path, status: :see_other
+    # redirect_to root_path, status: :see_other
+    respond_to do |format|
+      format.html { redirect_to root_path, status: :see_other, notice: "Article was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
+  # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :body, :status, images: [])
     end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,22 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    # redirect_to article_path(@article)
+    respond_to do |format|
+      format.html { redirect_to article_path(@article) }
+      format.json { head :no_content }
+    end
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    redirect_to article_path(@article), status: :see_other
+    # redirect_to article_path(@article), status: :see_other
+    respond_to do |format|
+      format.html { redirect_to article_path(@article), status: :see_other, notice: "Comment was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
